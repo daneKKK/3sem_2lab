4fdbddd939448715fff61a078eefcc396c4a3fe1
Daniil Alekseev <daniaalekseew@yandex.ru>
2022-09-20 16:11:08 +0300

    Introduced energy in Population header

--- Population.h
+++ Population.h
@@ -3,19 +3,20 @@
 class Population {
 private:
     int populationCount;
-    float mass;
+    float energy;
     float const massOfOneObject;
     std::string const name;
 public:
-    Population(int populationCount, float mass, float massOfOneObject);
+    Population(int populationCount, float massOfOneObject, float energy, std::string name);
     int getPopulationCount();
     void setPopulationCount(float mass);
-    float getMass();
-    void setMass(float mass);
+    float getEnergy();
+    void setEnergy(float energy);
     float getOneMass();
     std::string getName();
     virtual bool checkReproductiveAbility();
     virtual void reproduce();
     virtual bool checkDeath();
+    virtual void kill();
     virtual void eat();
 }
