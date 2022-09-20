f792f0c885e7b407ed0a6e9615bb9879d8efe2d6
Daniil Alekseev <daniaalekseew@yandex.ru>
2022-09-20 15:55:21 +0300

    Population header again

--- Population.h
+++ Population.h
@@ -1,15 +1,21 @@
+#include <string>
+
 class Population {
 private:
     int populationCount;
     float mass;
-    float const massOfOneObject = 1.;
+    float const massOfOneObject;
+    std::string const name;
 public:
-    Population(int populationCount);
+    Population(int populationCount, float mass, float massOfOneObject);
     int getPopulationCount();
     void setPopulationCount(float mass);
     float getMass();
     void setMass(float mass);
     float getOneMass();
-    bool
-    virtual checkReproductiveAbility();
+    std::string getName();
+    virtual bool checkReproductiveAbility();
+    virtual void reproduce();
+    virtual bool checkDeath();
+    virtual void eat();
 }
