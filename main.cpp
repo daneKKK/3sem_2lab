2e4bafb7ae490c691694bf62b7fd67050fa714b9
RufovD <rufov.da@phystech.edu>
2022-09-27 10:08:14 +0300

    добавил мэйн

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "Population.h"
+#include "Plants.h"
+#include "Bacterias.h"
+#include "BySolarEnergy.h"
+#include "CarnivoresBacterias.h"
+#include "FloweringPlants.h"
+#include "GymnospermPlants.h"
+
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
